85927f49afa7ed4c7878d7583135aabdd32f4929
Alexandre Ferreira <amellof@Alexandres-MacBook-Pro.local>
2020-12-03 12:45:30 -0300

    Adicionado funcao Soma()

--- calc.c
+++ calc.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+int soma(int x, int y) {
+  return x + y;
+}
+
 int main() {
   int a, b;
 
   scanf("%d", &a);
   scanf("%d", &b);
 
-  printf("Soma = %d\n\n", a+b);
+  printf("Soma = %d\n\n", soma(a,b));
 
   return 0;
 }
